5ba893cef461afc999eff080a4b5a6cb51dcee98
Wong Yi Jie <55955482+yjwong1999@users.noreply.github.com>
2023-07-09 19:13:06 +0800

    Add files via upload

--- reptile.sh
+++ reptile.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+###############################################################################################
+# cleansing unused files/directories
+###############################################################################################
+# remove all pointrcnn files with reptile name
+cd  output/custom_models
+find . -type d -name "*reptile*" -exec rm -rf {} +
+
+# remove previous round of pointrcnn to prevent clash
+rm -r -v "pointrcnn"
+
+# create a copy of pretrained model
+rm -r "pointrcnn_pretrained"
+cp -r "pointrcnn_pretrained (copy)" "pointrcnn_pretrained"
+cd ../../
+
+
+###############################################################################################
+# now lets start
+###############################################################################################
 # convert raw data to openpcdet format (this will create data/reptile, data/reptile_2, ...)
 if [ ! -d "data/reptile" ]; then
   python3 convert_raw_data_for_reptile.py --dir "data_raw/Mimos Dataset 2023/labels"
@@ -9,8 +28,9 @@ echo -e "There are $count Reptile Training Dataset \n"
 
 # define hyperparameters
 epochs=10
-outer_loop=50
-innter_loop=$count
+outer_loop=100
+inner_loop=$count
+epsilon=1.0
 
 # outer loop
 for i in $(seq 1 $outer_loop)
@@ -19,7 +39,7 @@ do
   # (i.e.: '"/xxx/xxx/model" "/xxx/xxx/model_2" "/xxx/xxx/model_3" ')
   all_model=''
   
-  for j in $(seq 1 $innter_loop)
+  for j in $(seq 1 $inner_loop)
   do
     # all reptile data are renamed as data/custom for generalizability
     if [ $j -eq 1 ]
@@ -63,12 +83,12 @@ do
   # get reptile model (saved in output/custom_models/pointrcnn_pretrained)
   mv "data/reptile" "data/custom"
   cd tools
-  python3 reptile.py --cfg_file cfgs/custom_models/pointrcnn.yaml --ckpts $all_model --epoch_id $epochs
+  python3 reptile.py --cfg_file cfgs/custom_models/pointrcnn.yaml --ckpts $all_model --epoch_id $epochs --pretrained_model ../output/custom_models/pointrcnn_pretrained/reptile.pth --epsilon $epsilon
   cd ../
   mv "data/custom" "data/reptile"
 
   # remove all task specific models in previous outer loop
-  for j in $(seq 1 $innter_loop)
+  for j in $(seq 1 $inner_loop)
   do
     if [ $j -eq 1 ]
     then
