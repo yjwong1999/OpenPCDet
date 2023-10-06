004d346a837b7394d89c85ddee620452ef7b1710
Wong Yi Jie <55955482+yjwong1999@users.noreply.github.com>
2023-10-07 05:16:31 +0800

    Update bash_train.sh

--- bash_train.sh
+++ bash_train.sh
@@ -32,15 +32,9 @@ CFG_FILE='tools/cfgs/custom_models/pointpillar.yaml'
 
 
 ################################
-# Fix the label path name in the json label, in case multiple did the labelling -> insonsistency
+# Fix the label path name in the json label, in case multiple people did the labelling -> insonsistency in root directory
 ################################
 python3 batch_fix_label.py --ply_dir $PLY_DIR --label_dir $LABEL_DIR
-
-
-################################
-# Augmentation
-################################
-python3 batch_augment.py --ply_dir $PLY_DIR --label_dir $LABEL_DIR
 
 
 ################################
