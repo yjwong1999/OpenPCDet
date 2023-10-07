fc3efd7688adb41701f6c317091fe18e297ed12c
Wong Yi Jie <55955482+yjwong1999@users.noreply.github.com>
2023-10-07 19:30:15 +0800

    Update bash_demo.sh

--- bash_demo.sh
+++ bash_demo.sh
@@ -16,15 +16,24 @@ conda init bash
 echo " "
 conda activate openpcdet
 
+################################
+# hyperparameters
+################################
+NAME="custom"
+LABEL_DIR="data_raw/techpartnerfile/techpartnerfile_label"
+PLY_DIR="data_raw/techpartnerfile/preprocessed_techpartnerfile-ply"
+
+#CFG_FILE='tools/cfgs/custom_models/pointrcnn.yaml'
+CFG_FILE='tools/cfgs/custom_models/pointpillar.yaml'
+#CFG_FILE='tools/cfgs/custom_models/pv_rcnn.yaml'
+
 
 ################################
 # Run demo.py
 ################################
 cd tools
 
-python demo.py --cfg_file cfgs/custom_models/pointrcnn.yaml --ckpt ../output/custom_models/pointrcnn/default/ckpt/checkpoint_epoch_300.pth --data_path "../data/custom/points" --ext .npy
-
-#python demo.py --cfg_file cfgs/custom_models/pointpillar.yaml --ckpt ../output/custom_models/pointpillar/default/ckpt/checkpoint_epoch_300.pth --data_path "../data/custom/points" --ext .npy
+python demo.py --cfg_file ${CFG_FILE:6:1000} --ckpt ../output/custom_models/pointrcnn/default/ckpt/checkpoint_epoch_100.pth --data_path "../data/custom/points" --ext .npy
 
 # "../data/custom/points/RF 010_R0C20_F_Snap3D_part1.npy"
 
